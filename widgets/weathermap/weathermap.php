1b9fe53880cbc69665c2449fd2071fb4da67ce52
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2020-04-01 22:01:26 +0200

    fixed weather map display

--- widgets/weathermap/weathermap.php
+++ widgets/weathermap/weathermap.php
@@ -1,17 +1,19 @@
 <style>
     #weathermapdiv{
-        height:550px;
-        width:512px;
-        overflow:hidden;
-    }
-    #weathermap{
         padding:10px;
-        width:100%;
         height:100%;
+        width:100%;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+    }
+    #weathermapp{
+        overflow:hidden;
         border-radius:20px;
-        
+        height:472px;
+        width:550px;
     }
 </style>
 <div id="weathermapdiv">
-    <iframe id="weathermap" src="https://gadgets.buienradar.nl/gadget/zoommap/?lat=52.95929&lng=5.91854&overname=2&zoom=11&naam=heerenveen&size=3&voor=1" scrolling=no width=550 height=512 frameborder=no></iframe>
+    <iframe id="weathermapp" src="https://gadgets.buienradar.nl/gadget/zoommap/?lat=52.88&lng=5.88&overname=2&zoom=11&naam=heerenveen&size=3&voor=1" scrolling=no frameborder=no></iframe>
 </div>
