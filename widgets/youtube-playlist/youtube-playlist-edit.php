54a73a2061364f557abc942c22e24239ffb64821
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2020-04-03 13:58:10 +0200

    neatened video/playlist ID input up

--- widgets/youtube-playlist/youtube-playlist-edit.php
+++ widgets/youtube-playlist/youtube-playlist-edit.php
@@ -2,6 +2,8 @@
     .youtubeplaylistform{
         font-size:175%;
         padding:10px;
+        padding-top:2px;
+        
     }
     .ytplaylist{
         height:100%;
@@ -13,6 +15,23 @@
         height:100%;
         width:80%;
     }
+    .ytplaylistbtn{
+        cursor:pointer;
+        border: 1px solid transparent;
+        border-radius:5px;
+        background-color:rgba(135, 205, 155, 0.5);
+    }
+    .ytplaylistbtn:hover{
+        background-color: rgba(135, 205, 155, 0.8);
+    }
+    .youtubeplaylistinput{
+        padding:2px;
+        border: 1px solid rgba(135, 205, 155, 0.8);
+        border-radius:5px;
+    }
+    .youtubeplaylistinput:focus{
+        outline:none;
+    }
 </style>
 <?php
 $ytpfilePath = getcwd()."\widgets\youtube-playlist\piles\playlist.txt";
@@ -21,10 +40,10 @@ $playlistid = fread($ytpfp, filesize($ytpfilePath));
 fclose($ytpfp);
 ?>
 <div class="ytplaylist"><iframe class="youtubeplaylistplayer" src="https://www.youtube.com/embed/videoseries?amp;list=<?=$playlistid?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-<h1>Insert a YouTube Playlist ID here</h1>
+<h3>Insert a YouTube Playlist ID here</h3>
 <form method="post" class="youtubeplaylistform">
     <input type="text" name="playlist" class="youtubeplaylistinput" value=<?=$playlistid?>>
-    <button type="submit">Save</button>
+    <button type="submit" class="ytplaylistbtn">Save</button>
 </form>
 <script>
     $(".youtubeplaylistform").submit(function(e) {
--- widgets/youtube-video/youtube-video-edit.php
+++ widgets/youtube-video/youtube-video-edit.php
@@ -2,6 +2,8 @@
     .youtubevideoform{
         font-size:175%;
         padding:10px;
+        padding-top:2px;
+        
     }
     .ytvideo{
         height:100%;
@@ -13,6 +15,23 @@
         height:100%;
         width:80%;
     }
+    .ytvideobtn{
+        cursor:pointer;
+        border: 1px solid transparent;
+        border-radius:5px;
+        background-color:rgba(135, 205, 155, 0.5);
+    }
+    .ytvideobtn:hover{
+        background-color: rgba(135, 205, 155, 0.8);
+    }
+    .youtubevideoinput{
+        padding:2px;
+        border: 1px solid rgba(135, 205, 155, 0.8);
+        border-radius:5px;
+    }
+    .youtubevideoinput:focus{
+        outline:none;
+    }
 </style>
 <?php
 $ytvfilePath = getcwd()."\widgets\youtube-video\piles\pideo.txt";
@@ -21,10 +40,10 @@ $videoid = fread($ytvfp, filesize($ytvfilePath));
 fclose($ytvfp);
 ?>
 <div class="ytvideo"><iframe class="youtubevideoplayer" src="https://www.youtube.com/embed/<?=$videoid?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-<h3>Insert a YouTube Video ID here</h3>
+<h3 style="margin:0px;">Insert a YouTube Video ID here</h3>
 <form method="post" class="youtubevideoform">
     <input type="text" name="video" class="youtubevideoinput" value=<?=$videoid?>>
-    <button type="submit">Save</button>
+    <button class="ytvideobtn" type="submit">Save</button>
 </form>
 <script>
     $(".youtubevideoform").submit(function(e) {
@@ -32,8 +51,8 @@ fclose($ytvfp);
         var youtubevideoLink = $(".youtubevideoinput").val(); //je input element
 
         $.post('/widgets/youtube-video/youtube-video-ajax.php', {youtubevideoLink: youtubevideoLink}, function(data) {
-        // stuff to do after saving, like saying "link saved!"
-        $(".ytvideo").html('<iframe class="youtubevideoplayer" src="https://www.youtube.com/embed/'+data+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+            // stuff to do after saving, like saying "link saved!" 
+            $(".ytvideo").html('<iframe class="youtubevideoplayer" src="https://www.youtube.com/embed/'+data+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
         });
     });
 </script>
