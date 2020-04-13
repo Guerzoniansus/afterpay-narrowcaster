c041bfeeccd05e58dba1e0cce3cbebed6dadaed9
Jortdeut <jort.van.deutekom@student.nhlstenden.com>
2020-04-13 21:03:39 +0200

    Birthday widget die goed werkt
    Op slideshow laat hij het niet zien, anders wel.  Makkelijke manier op op wel of niet laten zien te komen.

--- widgets/birthday/birthday-edit.php
+++ widgets/birthday/birthday-edit.php
@@ -6,6 +6,7 @@ $employees = $db->getBirthdayEmployees();
 
 ?>
 <div style="width: 100%; height: 100%; overflow: hidden; padding: 20px; font-size: 15px; text-align: center;">
+<a href="birthdaysettings.php" target="_blank">Klik hier om verjaardagen wel of niet te laten zien</a>
 <h1> Verjaardagen 🎉 </h1>
 <? foreach ($employees as $employee): ?>
 <h5 style="background-color: rgba(77,167,122,1);"><?= $employee->employeeName ?></h5>
