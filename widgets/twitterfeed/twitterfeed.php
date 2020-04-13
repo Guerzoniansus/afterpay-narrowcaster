00c89f0c94566277dc86ee1b47f95fd00698ca37
Jortdeut <jort.van.deutekom@student.nhlstenden.com>
2020-04-13 13:12:19 +0200

    Twitter feed, die niet heel mooi lukt
    Past niet perfect bij de andere widgets, en als je 2 widgets horizontaal doet past het niet in het scherm

---
+++ widgets/twitterfeed/twitterfeed.php
@@ -0,0 +1,12 @@
+<!-- Twitter feed van afterpay op de website -->
+<a class="twitter-timeline" 
+data-width=100% 
+data-height=100% 
+data-theme="light" 
+align="center"
+
+href="https://twitter.com/AfterPay?ref_src=twsrc%5Etfw">Tweets by AfterPay</a> 
+<script 
+async src="https://platform.twitter.com/widgets.js" 
+charset="utf-8">
+</script>
