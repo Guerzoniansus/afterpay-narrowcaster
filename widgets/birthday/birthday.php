e49766aa9405d4d6a1265684c342de397987b282
Guerzoniansus <weezimonkey@gmail.com>
2020-04-14 14:12:29 +0200

    Finished birthday

--- widgets/birthday/birthday.php
+++ widgets/birthday/birthday.php
@@ -4,12 +4,25 @@ require_once 'DB.php';
 $db = new DB();
 $employees = $db->getBirthdayEmployees();
 
+if (!function_exists("calculateAge")) {
+	function calculateAge($birthday) {
+		$date  = new DateTime($birthday);
+		$now = new DateTime();
+		$difference = $now->diff($date);
+		// y means years
+		return $difference->y;
+	}
+}
+
 ?>
+
+
 <div style="height: 100%; width: 100%; overflow: hidden; padding: 20px; font-size: 15px; text-align: center;">
 <h1> Birthdays 🎉 </h1>
+    <br>
 <? foreach ($employees as $employee): ?>
-<h2 style="background-color: rgba(77,167,122,1);"><?= $employee->employeeName ?></h2>
-<p>Functie:   <?= $employee->function ?></p>
-<p><?= $employee->birthday ?></p>
+    <h1 style="background-color: #87CD9B;"><?= $employee->employeeName ?></h1>
+    <h2>Age: <?= calculateAge($employee->birthday) ?></h2>
+    <p>Functie:   <?= $employee->function ?></p>
 <? endforeach; ?>
 </div>
